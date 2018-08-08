3007374fc7a6d5897f30cbaa1c32379311cf5e5b
Pan Cheng <pchengeducation@hotmail.com>
2018-08-08 17:26:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 // Code your solution in this file!
 function returnFirstTwoDrivers(){
-  const variable = (function (drivers){return drivers[0], driver[1]})
+  const variable = (function (drivers){return drivers[0], drivers[1]})
 }
