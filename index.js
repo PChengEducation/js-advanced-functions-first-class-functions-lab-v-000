1ea0d95ef210b8d4bffcb45001d6eca943e1f0a3
Pan Cheng <pchengeducation@hotmail.com>
2018-08-08 17:33:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 // Code your solution in this file!
-function returnFirstTwoDrivers(){
-  const variable = (function (drivers){return drivers[0], drivers[1]})
+const returnFirstTwoDrivers = function(drivers){
+  newDrivers = drivers.slice(0,2);
+  return newDrivers;
 }
